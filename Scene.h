4869f61007aeeaedb9813e4d67028ab075c5ac9e
Vaduva Paula <paula.alexandra.vaduva@gmail.com>
2026-03-24 00:17:32 +0200

    add road and static objects

--- Scene.h
+++ Scene.h
@@ -8,4 +8,12 @@ void drawSkybox(float size);
 void drawGround(float size);
 void drawRelief(float size);
 
+void drawCircuit(float innerRadius, float outerRadius, int segments);
+float getTerrainHeight(float x, float z);
+
+void drawTree(float x, float z);
+void drawBuilding(float x, float z, float width, float height, float depth);
+void drawStaticObjects();
+bool isPointOnCircuit(float x, float z, float innerR, float outerR);
+
 #endif
